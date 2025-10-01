16a802535c79e3bf9d23ce88ed5c256f62e40c96
gustavooarantes <profissional.gustavoa@gmail.com>
2025-10-01 06:50:43 -0300

    #02[feat] - Created the JPA repository for handling the Post entity

---
+++ src/main/java/com/gustavooarantes/post_api/repository/PostRepository.java
@@ -0,0 +1,6 @@
+package com.gustavooarantes.post_api.repository;
+
+import com.gustavooarantes.post_api.model.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepository extends JpaRepository<Post, Long> {}
